7b43da2ea53eb474b744cfcfd729f62c6f5a7e92
ch <2564510610@qq.com>
2020-01-06 10:17:21 +0800

    login_actions

--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,6 @@
+import api from './api'
+import { requestPost } from '@/utils/request'
+
 // 张慧敏 api
 
 // 李晓超 api
@@ -5,3 +8,6 @@
 // 王红涛 api
 
 // 崔亚俊 api
+export const login = (options) => {
+    return requestPost(api.Login, options)
+}
