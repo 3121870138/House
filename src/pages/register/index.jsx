6736df66f3342477e4ff6b2afc01a3f8bc0fe782
wht <3054955228@qq.com>
2020-01-06 09:04:04 +0800

    '注册页面布局'

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './styles.less'
+import { Input, Button, } from 'antd';
+export default class extends React.Component {
 
-export default class extends React.PureComponent {
+
     render() {
         return (
             <div className='register'>
-                register
+                <div className="regUser">
+                    <p><Input   placeholder="username"/></p>
+                    <p><Input.Password   placeholder="password"/></p>
+                    <p><Button type="primary">注册</Button></p>
+                </div>
             </div>
         )
     }
