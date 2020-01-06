819b2ca88fa0dd3ad0262521e8caf66730776e38
ch <2564510610@qq.com>
2020-01-06 09:30:17 +0800

    title

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -5,7 +5,23 @@ export default class extends React.PureComponent {
     render() {
         return (
             <div className='login'>
-                login
+                <div className="login-form">
+                    <div className="logo">大图</div>
+                    <div className="form-sub">
+                        <div className="login-input input-username">
+                            <span>用户名: </span>
+                            <input type="text"/>
+                        </div>
+                        <div className="login-input input-password">
+                            <span>密码: </span>
+                            <input type="text"/>
+                        </div>
+                        <div className="login-input input-submit">
+                            <button>登录</button>
+                            <button>重置</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
